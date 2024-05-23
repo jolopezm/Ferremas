b305354818573be7f87d06151ce15b47239fab30
jolopezm <127951621+jolopezm@users.noreply.github.com>
2024-05-23 14:54:25 -0400

    fisrt commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class Producto(BaseModel):
+    nombre: str
+    marca: str
+    precio: int
+    stock: int
+    descripcion: Optional[str]
+
+@app.get('/')
+def index():
+    return {"message": "hola."}
+
+@app.get('/producto/{id}')
+def mostrar_producto(id: int):
+    return {"data": id}
+
+@app.post('/insertar-producto')
+def insertar_producto(producto: Producto):
+    return {"message": f"producto {producto.nombre} ha sido insertado."}
